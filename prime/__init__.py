a29b2cb787dbfba9cec4b033ab4e791905ba9e0f
Bapan Biswas <babiswas@adobe.com>
2022-02-13 11:32:46 +0530

    commited

--- prime/__init__.py
+++ prime/__init__.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-print("Prime block executed")
-print("Another print")
-=======
-<<<<<<< HEAD
-print("Prime block executed")
-=======
-print("prime task")
->>>>>>> 21.5
->>>>>>> 69c4d6e879e55fb1fd25a2bc2c7632cd3a1ff5f8
+print("hello world")
+print("hello world")
+print("hello world")
+
