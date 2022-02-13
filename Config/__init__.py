9832b8466b2f18fd8dde4a14a0581bf6dcd7fdb9
Bapan Biswas <babiswas@adobe.com>
2022-02-13 10:31:37 +0530

    commited

--- Config/__init__.py
+++ Config/__init__.py
@@ -2,6 +2,7 @@ from requests import post
 from Config.Config import OAUTH
 
 print("Running Config block")
+print("Running config block")
 
 res=post(OAUTH.END_POINT,params={"client_id":OAUTH.CLIENT_ID,"client_secret":OAUTH.CLIENT_SECRET,"refresh_token":OAUTH.REFRESH_TOKEN},headers={"Content-type":"application/x-www-form-urlencoded"})
 OAUTH.TOKEN=res.json()["access_token"]
